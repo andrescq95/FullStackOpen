30dffc9c26cd4217a43836e3f3f07dcfd9dba1fe
Andrés Camacho <andres.camacho@Andres-Camacho.local>
2025-08-06 14:04:58 -0600

    Completed part 1.6

--- Part_1/unicafe/src/App.jsx
+++ Part_1/unicafe/src/App.jsx
@@ -1,15 +1,51 @@
 import { useState } from 'react'
 
+const Header = ({ title }) => <h1>{title}</h1>
+
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+
+const Display = (props) => {
+  return (
+    <>
+      <p>
+        Good: {props.statistics[0]} <br />
+        Neutral: {props.statistics[1]} <br />
+        Bad: {props.statistics[2]}
+      </p>
+    </>
+  )
+}
+
 const App = () => {
-  // save clicks of each button to its own state
+  const formHeaders = ['give feedback', 'statistics']
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const handleGoodClick = () => {
+    const updatedGood = good + 1
+    setGood(updatedGood)
+  }
+
+  const handleNeutralClick = () => {
+    const updatedNeutral = neutral + 1
+    setNeutral(updatedNeutral)
+  }
+
+  const handleBadClick = () => {
+    const updatedBad = bad + 1
+    setBad(updatedBad)
+  }
+
   return (
-    <div>
-      code here
-    </div>
+    <>
+      <Header title = {formHeaders[0]} />
+      <Button onClick={handleGoodClick} text='good' />
+      <Button onClick={handleNeutralClick} text='neutral' />
+      <Button onClick={handleBadClick} text='bad' />
+      <Header title = {formHeaders[1]} />
+      <Display statistics = {[good, neutral, bad]}/>
+    </>
   )
 }
 
