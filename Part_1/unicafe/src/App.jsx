71d926d0694f1c1d4ed77ea68f7d53df9726a467
Andrés Camacho <andres.camacho@Andres-Camacho.local>
2025-08-06 18:27:57 -0600

    Completed part 1.7

--- Part_1/unicafe/src/App.jsx
+++ Part_1/unicafe/src/App.jsx
@@ -10,7 +10,10 @@ const Display = (props) => {
       <p>
         Good: {props.statistics[0]} <br />
         Neutral: {props.statistics[1]} <br />
-        Bad: {props.statistics[2]}
+        Bad: {props.statistics[2]} <br />
+        All: {props.statistics[3]} <br />
+        Average: {props.statistics[4]} <br />
+        Positive: {props.statistics[5] } % <br />
       </p>
     </>
   )
@@ -21,20 +24,50 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+  const [average, setAverage] = useState(0);
+  const [positive, setPositive] = useState(0);
 
   const handleGoodClick = () => {
+    //Update the total of Good clicks
     const updatedGood = good + 1
     setGood(updatedGood)
+    //Update the overall total of clicks
+    const updatedTotal = updatedGood + neutral + bad
+    setTotal(updatedTotal);
+    //Calculate the average score (Score / Total)
+    const totalScore = updatedGood * 1 + neutral * 0 + bad * -1;
+    setAverage((totalScore) / (updatedTotal > 0 ? updatedTotal : 1));
+    //Calculate the percentage of positive feedback
+    setPositive(updatedGood / updatedTotal * 100);
   }
 
   const handleNeutralClick = () => {
+    //Update the total of Neutral clicks
     const updatedNeutral = neutral + 1
     setNeutral(updatedNeutral)
+    //Update the overall total of clicks
+    const updatedTotal = updatedNeutral + good + bad
+    setTotal(updatedTotal)
+    //Calculate the average score (Score / Total)
+    const totalScore = updatedNeutral * 0 + good * 1 + bad * -1;
+    setAverage((totalScore) / (updatedTotal > 0 ? updatedTotal : 1));
+    //Calculate the percentage of positive feedback
+    setPositive(good / updatedTotal * 100);
   }
 
   const handleBadClick = () => {
-    const updatedBad = bad + 1
-    setBad(updatedBad)
+    //Update the total of Bad clicks
+    const updatedBad = bad + 1;
+    setBad(updatedBad);
+    //Update the overall total of clicks
+    const updatedTotal = updatedBad + good + neutral
+    setTotal(updatedTotal);
+    //Calculate the average score (Score / Total)
+    const totalScore = updatedBad * -1 + neutral * 0 + good * 1;
+    setAverage((totalScore) / (updatedTotal > 0 ? updatedTotal : 1));
+    //Calculate the percentage of positive feedback
+    setPositive(good / updatedTotal * 100);
   }
 
   return (
@@ -44,7 +77,7 @@ const App = () => {
       <Button onClick={handleNeutralClick} text='neutral' />
       <Button onClick={handleBadClick} text='bad' />
       <Header title = {formHeaders[1]} />
-      <Display statistics = {[good, neutral, bad]}/>
+      <Display statistics = {[good, neutral, bad, total, average, positive]}/>
     </>
   )
 }
