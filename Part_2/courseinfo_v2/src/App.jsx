749e9dfe7605df27030d0bd19eaa8f4dbcf5b986
Andrés Camacho <andres.camacho@Andres-Camacho.local>
2025-08-15 09:36:14 -0600

    Completed part 2.2

--- Part_2/courseinfo_v2/src/App.jsx
+++ Part_2/courseinfo_v2/src/App.jsx
@@ -7,18 +7,19 @@ const Content = ({parts}) => (
     <Part part = {parts[0].name} exercises = {parts[0].exercises}/>
     <Part part = {parts[1].name} exercises = {parts[1].exercises}/>
     <Part part = {parts[2].name} exercises = {parts[2].exercises}/>
+    <Part part = {parts[3].name} exercises = {parts[3].exercises}/>
   </>
 )
 
 const Part = ({part, exercises}) => (
   <p>
-      {part} {exercises}
+    {part} {exercises}
   </p>
 )
 
 const Total = (props) => (
   <p>
-    Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}
+    <b>Total of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises + props.parts[3].exercises}</b>
   </p>
 )
 
@@ -36,23 +37,28 @@ const App = () => {
     name: 'Half Stack application development',
     parts: [
       {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
+      name: 'Fundamentals of React',
+      exercises: 10,
+      id: 1
       },
       {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
+      name: 'Using props to pass data',
+      exercises: 7,
+      id: 2
       },
       {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
+      name: 'State of a component',
+      exercises: 14,
+      id: 3
+      },
+      {
+      name: 'Redux',
+      exercises: 11,
+      id: 4
       }
     ]
   }
-  return <Course course={course} />
+return <Course course={course} />
 }
 
 export default App
