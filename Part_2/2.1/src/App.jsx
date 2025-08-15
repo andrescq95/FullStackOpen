0a5f964dd2016d86db0f808fbafdd182dd00391d
Andrés Camacho <andres.camacho@Andres-Camacho.local>
2025-08-14 18:20:24 -0600

    Completed Part 2.1

--- Part_2/2.1/src/App.jsx
+++ Part_2/2.1/src/App.jsx
@@ -1,14 +1,59 @@
+const Header = ({name}) => (
+      <h1>{name}</h1>
+)
 
-const Header = ({ title }) => <h1>{title}</h1>
+const Content = ({parts}) => (
+      <>
+      <Part part = {parts[0].name} exercises = {parts[0].exercises}/>
+      <Part part = {parts[1].name} exercises = {parts[1].exercises}/>
+      <Part part = {parts[2].name} exercises = {parts[2].exercises}/>
+      </>
+)
+
+const Part = ({part, exercises}) => (
+      <p>
+          {part} {exercises}
+      </p>
+)
+
+const Total = (props) => (
+      <p>
+        Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}
+      </p>
+)
+
+const Course = ({course}) => (
+    <>
+    <Header name = {course.name} />
+    <Content parts = {course.parts} />
+    <Total parts = {course.parts} />
+    </>
+)
 
 const App = () => {
-  const header = 'Part 2'
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
 
-  return (
-    <>
-      <Header title = {header} />
-    </>
-  )
+  return <Course course={course} />
 }
 
 export default App
