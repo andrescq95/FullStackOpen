c8e782d742c5bbdea1887597a498ace5bf4ea1c9
Andrés Camacho <andres.camacho@Andres-Camacho.local>
2025-08-14 20:33:05 -0600

    Completed Part 2.4

---
+++ Part_2/courseinfo_2/src/App.jsx
@@ -0,0 +1,98 @@
+const Header = ({name}) => {
+  return (
+      <h2>{name}</h2>
+  )
+}
+
+const Content = ({part}) => {
+  return (
+    <>
+      {part.map(part => (
+        <Part key={part.id} part={part.name} exercises={part.exercises} />
+      ))}
+    </>
+  )
+}
+
+const Part = ({part, exercises}) => (
+  <p>
+    {part} {exercises}
+  </p>
+)
+
+const Total = ( {course} ) => {
+    //sum starts at 0, .reduce iterates through parts with the 'part' and adds the exercises to the sum
+  return (
+    <p>
+      <b>Total of exercises: {course.parts.reduce((sum, part) => sum + part.exercises, 0)}</b>
+    </p>
+  )
+}
+
+const Course = ({courses}) => {
+  return (
+    <>
+      <h1>Web development curriculum</h1>
+      <Header name = {courses[0].name} />
+      <Content part={courses[0].parts} />
+      <Total course = {courses[0]} />
+      <Header name = {courses[1].name} />
+      <Content part={courses[1].parts} />
+      <Total course = {courses[1]} />
+    </>
+  )
+}
+
+const App = () => {
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+  return (
+    <div>
+      <Course courses={courses} />
+    </div>
+  )
+}
+
+export default App
