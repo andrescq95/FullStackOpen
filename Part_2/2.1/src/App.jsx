902536f33c8dcd2982fa48a25e134328c69c2e75
Andrés Camacho <andres.camacho@Andres-Camacho.local>
2025-08-14 18:24:58 -0600

    margins

--- Part_2/2.1/src/App.jsx
+++ Part_2/2.1/src/App.jsx
@@ -4,16 +4,16 @@ const Header = ({name}) => (
 
 const Content = ({parts}) => (
   <>
-  <Part part = {parts[0].name} exercises = {parts[0].exercises}/>
-  <Part part = {parts[1].name} exercises = {parts[1].exercises}/>
-  <Part part = {parts[2].name} exercises = {parts[2].exercises}/>
-  <Part part = {parts[3].name} exercises = {parts[3].exercises}/>
+    <Part part = {parts[0].name} exercises = {parts[0].exercises}/>
+    <Part part = {parts[1].name} exercises = {parts[1].exercises}/>
+    <Part part = {parts[2].name} exercises = {parts[2].exercises}/>
+    <Part part = {parts[3].name} exercises = {parts[3].exercises}/>
   </>
 )
 
 const Part = ({part, exercises}) => (
   <p>
-      {part} {exercises}
+    {part} {exercises}
   </p>
 )
 
@@ -24,42 +24,41 @@ const Total = (props) => (
 )
 
 const Course = ({course}) => (
-<>
-<Header name = {course.name} />
-<Content parts = {course.parts} />
-<Total parts = {course.parts} />
-</>
+  <>
+    <Header name = {course.name} />
+    <Content parts = {course.parts} />
+    <Total parts = {course.parts} />
+  </>
 )
 
 const App = () => {
-const course = {
-id: 1,
-name: 'Half Stack application development',
-parts: [
-  {
-    name: 'Fundamentals of React',
-    exercises: 10,
-    id: 1
-  },
-  {
-    name: 'Using props to pass data',
-    exercises: 7,
-    id: 2
-  },
-  {
-    name: 'State of a component',
-    exercises: 14,
-    id: 3
-  },
-  {
-    name: 'Redux',
-    exercises: 11,
-    id: 4
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      },
+      {
+        name: 'Redux',
+        exercises: 11,
+        id: 4
+      }
+    ]
   }
-]
-}
-
-return <Course course={course} />
+  return <Course course={course} />
 }
 
 export default App
