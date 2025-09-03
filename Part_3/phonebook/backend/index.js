16d0648bf0ef34a322ec7c1da2098ce8f7b422b9
Andrés Camacho <andres.camacho@Andres-Camacho.local>
2025-09-03 17:35:15 -0600

    Completed parts 3.3 - 3.6

--- Part_3/phonebook/backend/index.js
+++ Part_3/phonebook/backend/index.js
@@ -27,60 +27,73 @@ let contacts = [
     }
 ]
 
+//Main page
 app.get('/', (request, response) => {
   response.send('<h1>Phonebook Main Page</h1>')
 })
 
+//Info page
 app.get('/info', (request, response) => {
   const date = new Date()
   response.send(`<p>Phonebook has info for ${contacts.length} people</p><p>${date}</p>`)
 })
 
+//Get all contacts
 app.get('/api/contacts', (request, response) => {
   response.json(contacts)
 })
 
+//Get a single contact by ID
 app.get('/api/contacts/:id', (request, response) => {
   const id = request.params.id
-  const note = notes.find(note => note.id === id)
+  const contact = contacts.find(contact => contact.id === id)
 
-  if (note) {
-    response.json(note)
+  if (contact) {
+    response.json(contact)
   } else {
     response.status(404).end()
   }
 })
 
+//Random ID generator
 const generateId = () => {
-  const maxId = notes.length > 0
-    ? Math.max(...notes.map(n => Number(n.id)))
-    : 0
-  return String(maxId + 1)
+  return String(Math.floor(Math.random() * (1000 - 5) + 5))
 }
 
+// Normalize name by trimming spaces and converting to lowercase
+const normalizeName = (name) => {
+    return name.toLowerCase().trim().replace(/\s+/g, ' ')
+}
+
+//Add a new contact
 app.post('/api/contacts', (request, response) => {
   const body = request.body
 
-  if (!body.content) {
+  if (!body.name || !body.number) {
     return response.status(400).json({
-      error: 'content missing'
+      error: 'Name or Number are missing'
     })
   }
-
-  const note = {
-    content: body.content,
-    important: body.important || false,
+  else if (contacts.find(contact => (normalizeName(contact.name) === normalizeName(body.name)))) {
+    return response.status(400).json({
+      error: 'Name must be unique'
+    })
+  }
+  const contact = {
+    name: body.name,
+    number: body.number,
     id: generateId(),
   }
 
-  notes = notes.concat(note)
+  contacts = contacts.concat(contact)
 
-  response.json(note)
+  response.json(contact)
 })
 
+//Delete a contact by ID
 app.delete('/api/contacts/:id', (request, response) => {
   const id = request.params.id
-  notes = notes.filter(note => note.id !== id)
+  contacts = contacts.filter(contact => contact.id !== id)
 
   response.status(204).end()
 })
