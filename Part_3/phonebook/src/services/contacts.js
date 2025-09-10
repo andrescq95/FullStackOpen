13480ca7dc4e55d5cb6ef13421d493141b07e467
Andrés Camacho <andres.camacho@Andres-Camacho.local>
2025-09-10 13:26:58 -0600

    Application to the internet setup - Frontend

--- Part_3/phonebook/src/services/contacts.js
+++ Part_3/phonebook/src/services/contacts.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/api/contacts'
+const baseUrl = '/api/contacts'
 
 const getAllContacts = () => {
   const request = axios.get(baseUrl)
