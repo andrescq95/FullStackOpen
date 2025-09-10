68be979be8bc3eb412d400c770266a39f2b8d8aa
Andrés Camacho <andres.camacho@Andres-Camacho.local>
2025-09-10 13:54:56 -0600

    Application to the internet setup

--- Part_3/phonebook/backend/index.js
+++ Part_3/phonebook/backend/index.js
@@ -30,6 +30,7 @@ let contacts = [
 app.use(express.json())
 app.use(cors())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
+app.use(express.static('dist'))
 
 morgan.token('body', (req, res) => {
   return JSON.stringify(req.body) || null;
