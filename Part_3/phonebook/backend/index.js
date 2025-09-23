7f455754bd37367d1fa47219665baa7a1470b504
Andrés Camacho <andres.camacho@Andres-Camacho.local>
2025-09-23 15:10:16 -0600

    Streamlining deploying of the frontend + Proxy

--- Part_3/phonebook/backend/index.js
+++ Part_3/phonebook/backend/index.js
@@ -2,7 +2,6 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 const PORT = process.env.PORT || 3001
-const cors = require('cors')
 
 let contacts = [
     {
@@ -28,7 +27,6 @@ let contacts = [
 ]
 
 app.use(express.json())
-app.use(cors())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 app.use(express.static('dist'))
 
